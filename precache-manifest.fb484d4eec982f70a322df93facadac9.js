bfe13cd4b13b1021af0ec75a8f295275e2a02148
VieronikaHontar <ronnicka@gmail.com>
2019-04-25 15:07:53 +0300

    Updates

---
+++ precache-manifest.fb484d4eec982f70a322df93facadac9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd46b341138f98186b7fafd4bffe0fb",
+    "url": "/42143v/index.html"
+  },
+  {
+    "revision": "8c14d38b4b6fa1c10fc8",
+    "url": "/42143v/static/css/main.22bcaf1f.chunk.css"
+  },
+  {
+    "revision": "b4ccfd32652233a8acb7",
+    "url": "/42143v/static/js/2.a34c6397.chunk.js"
+  },
+  {
+    "revision": "8c14d38b4b6fa1c10fc8",
+    "url": "/42143v/static/js/main.ab070e43.chunk.js"
+  },
+  {
+    "revision": "cbb972de59dba422b91c",
+    "url": "/42143v/static/js/runtime~main.56e5d80c.js"
+  },
+  {
+    "revision": "447ef0d98e5a8fc6883a48b9bbb87622",
+    "url": "/42143v/static/media/avatar.447ef0d9.png"
+  }
+]);
